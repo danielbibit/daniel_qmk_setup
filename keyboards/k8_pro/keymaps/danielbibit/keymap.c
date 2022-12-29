964d8b398680adf93e6f5c7897c138ef733feb8f
Daniel Moraes <danielbibit@gmail.com>
2022-12-29 06:30:42 -0300

    Add exclusive mouse layer

--- keyboards/k8_pro/keymaps/danielbibit/keymap.c
+++ keyboards/k8_pro/keymaps/danielbibit/keymap.c
@@ -6,6 +6,7 @@ enum layers{
   WIN_FN,
   DAN_WIN,
   DAN_WIN_FN,
+  MOUSE_FN,
   CONFIG
 };
 
@@ -39,13 +40,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
 
     [DAN_WIN] = LAYOUT_ansi_87(
-         KC_CAPS, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, KC_PSCR, KC_SCRL, KC_PAUS,
+         _______, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, KC_PSCR, KC_SCRL, KC_PAUS,
 
          KC_GRV, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_EQL, KC_BSPC, KC_INS, KC_HOME, KC_PGUP,
 
          LT(DAN_WIN_FN, KC_TAB), KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC, LT(DAN_WIN_FN, KC_BSLS), KC_DEL, KC_END, KC_PGDN,
 
-         MT(MOD_LCTL, KC_ESC), KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, MT(KC_NO, KC_ENT),
+         MT(MOD_LCTL, KC_ESC), KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, LT(MOUSE_FN, KC_ENT),
 
          KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT, KC_UP,
 
@@ -55,17 +56,30 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [DAN_WIN_FN] = LAYOUT_ansi_87(
          _______,  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
 
-         LCA(KC_T), KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, KC_DEL, KC_BTN1, KC_MS_U, KC_BTN2,
+         LCA(KC_T), KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, KC_DEL, _______, _______, _______,
 
-         LCA(KC_F1), KC_MPLY, KC_WH_U, KC_MPRV, KC_MNXT, _______, _______, _______, _______, LCA(KC_O), KC_PSCR, _______, KC_PGUP, LGUI(KC_L), KC_MS_L, KC_MS_D, KC_MS_R,
+         LCA(KC_F1), KC_MPLY, _______, KC_MPRV, KC_MNXT, _______, _______, _______, _______, LCA(KC_O), KC_PSCR, _______, KC_PGUP, LGUI(KC_L), _______, _______, _______,
 
-         KC_LCTL, KC_WH_L, KC_WH_D, KC_WH_R, KC_HOME, KC_END, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, _______, KC_PGDN, KC_INS,
+         KC_LCTL, _______, _______, _______, KC_HOME, KC_END, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, _______, KC_PGDN, KC_INS,
 
          KC_LSFT, KC_VOLD, KC_VOLU, LCA(KC_C), KC_MUTE, _______, _______, LCA(KC_M), _______, _______, _______, KC_RSFT, _______,
 
          KC_LCTL, KC_LGUI, KC_LALT, KC_LCTL, KC_RALT, _______, _______, KC_RCTL, _______, _______, _______
     ),
 
+    [MOUSE_FN] = LAYOUT_ansi_87(
+         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+
+         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+
+         _______, KC_BTN1, KC_MS_U, KC_BTN2, KC_WH_U, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+
+         _______, KC_MS_L, KC_MS_D, KC_MS_R, KC_WH_D, _______, _______, KC_BTN1, KC_BTN2, _______, _______, _______, _______,
+
+         _______, KC_WH_L, KC_WH_R, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+
+         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
+    ),
 
     [CONFIG] = LAYOUT_ansi_87(
          _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, RGB_TOG,
