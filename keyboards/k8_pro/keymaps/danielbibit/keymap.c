b1941c92c1f1bcb4d287ace7576fb3e676772218
Daniel Moraes <danielbibit@gmail.com>
2022-12-30 05:13:34 -0300

    Move home and end to <>

--- keyboards/k8_pro/keymaps/danielbibit/keymap.c
+++ keyboards/k8_pro/keymaps/danielbibit/keymap.c
@@ -44,13 +44,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
          KC_GRV, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_EQL, KC_BSPC, KC_INS, KC_HOME, KC_PGUP,
 
-         LT(DAN_WIN_FN, KC_TAB), KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC, LT(DAN_WIN_FN, KC_BSLS), KC_DEL, KC_END, KC_PGDN,
+         LT(DAN_WIN_FN, KC_TAB), KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC, KC_BSLS, KC_DEL, KC_END, KC_PGDN,
 
-         MT(MOD_LCTL, KC_ESC), KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, LT(MOUSE_FN, KC_ENT),
+         MT(MOD_LCTL, KC_ESC), KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, LT(DAN_WIN_FN, KC_ENT),
 
          KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT, KC_UP,
 
-         KC_NO, KC_LGUI, KC_LALT, KC_SPC, KC_RALT, KC_RGUI, MO(CONFIG), KC_NO, KC_LEFT, KC_DOWN, KC_RGHT
+         MO(DAN_WIN_FN), KC_LGUI, KC_LALT, LT(MOUSE_FN,KC_SPC), KC_RALT, KC_RGUI, MO(CONFIG), KC_NO, KC_LEFT, KC_DOWN, KC_RGHT
     ),
 
     [DAN_WIN_FN] = LAYOUT_ansi_87(
@@ -60,9 +60,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
          LCA(KC_F1), KC_MPLY, _______, KC_MPRV, KC_MNXT, _______, _______, _______, _______, LCA(KC_O), KC_PSCR, _______, KC_PGUP, LGUI(KC_L), _______, _______, _______,
 
-         KC_LCTL, _______, _______, _______, KC_HOME, KC_END, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, _______, KC_PGDN, KC_INS,
+         KC_LCTL, _______, _______, _______, _______, _______, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, _______, KC_PGDN, KC_INS,
 
-         KC_LSFT, KC_VOLD, KC_VOLU, LCA(KC_C), KC_MUTE, _______, _______, LCA(KC_M), _______, _______, _______, KC_RSFT, _______,
+         KC_LSFT, KC_VOLD, KC_VOLU, LCA(KC_C), KC_MUTE, _______, _______, LCA(KC_M), KC_HOME, KC_END, _______, KC_RSFT, _______,
 
          KC_CAPS, KC_LGUI, KC_LALT, KC_LCTL, KC_RALT, _______, _______, KC_RCTL, _______, _______, _______
     ),
